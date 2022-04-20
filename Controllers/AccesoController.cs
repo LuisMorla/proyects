36abdd8250682c6d2463cd1ba598344c9ac2f9eb
LuisMorla <Luis0045@outlook.es>
2022-04-20 03:14:34 -0400

    Segunda subida

--- Controllers/AccesoController.cs
+++ Controllers/AccesoController.cs
@@ -115,7 +115,7 @@ namespace PaginaDeRegistros.Controllers
                 Encoding enc = Encoding.UTF8;
                 byte[] result=hash.ComputeHash(enc.GetBytes(texto));
 
-                foreach(byte b in result)
+                foreach(byte b in result) 
                     sb.Append(b.ToString("x2"));
             }
             return sb.ToString();
